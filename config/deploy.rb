c02bad429df648f08867146389fae1287f38c0b1
trevorius <trevorius45@hotmail.com>
2020-11-16 22:18:58 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.14.1"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "Trevor_Kitchen"
+set :application, "Trevor_Kitchen_team2"
 set :repo_url, "https://github.com/week7VietEmanuellaJulienTrevor/weekend"
 
 # Default branch is :master
