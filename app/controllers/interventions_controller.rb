9a732dab137386da8ebf46aea3051d07055be8dd
trevorius <trevorius45@hotmail.com>
2020-11-18 10:46:51 -0500

    submit button creates new intervention

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -28,7 +28,7 @@ class InterventionsController < InheritedResources::Base
   private
 
     def intervention_params
-      params.require(:intervention).permit(:customer_id, :buidling_id, :battery_id, :column_id, :elevator_id, :employeeId, :start_date, :end_date, :result, :report, :status)
+      params.require(:interventions).permit(:customer_id, :buidling_id, :battery_id, :column_id, :elevator_id, :employeeId, :start_date, :end_date, :result, :report, :status)
     end
 
 end
