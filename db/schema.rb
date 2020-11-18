8dfce1901b37d417fa0033c88ea6146b8a935941
trevorius <trevorius45@hotmail.com>
2020-11-18 10:53:20 -0500

    added author column to intereventions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_220621) do
+ActiveRecord::Schema.define(version: 2020_11_18_154705) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -176,6 +176,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_220621) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author"
   end
 
   create_table "leads", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
