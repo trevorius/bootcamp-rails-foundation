eaf17e3a2d582ffc3416517b1792434a0cbd3113
trevorius <trevorius45@hotmail.com>
2020-11-23 21:03:32 -0500

    request for joke works if hardcoded api key

--- lib/elevator_media_display/elevator_media.rb
+++ lib/elevator_media_display/elevator_media.rb
@@ -3,5 +3,27 @@ module ElevatorMedia
         def self.getContent()
             "test string"
         end
+        def self.api_request(range)
+            require 'uri'
+            require 'net/http'
+            require 'openssl'
+            require 'json'
+
+            url = URI("https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw%2Cracist&idRange=#{range}&type=single%2Ctwopart")
+
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+            request = Net::HTTP::Get.new(url)
+            request["x-rapidapi-key"] = ENV['rapidapi_key']
+            request["x-rapidapi-host"] = 'jokeapi-v2.p.rapidapi.com'
+
+            response = http.request(request)
+
+            puts response.read_body
+            response.read_body
+            JSON.parse(response.read_body)
+        end
     end
 end 
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -1,10 +1,34 @@
 require 'elevator_media_display/elevator_media'
+require 'json'
 
 describe ElevatorMedia::Streamer do
     describe ".getContent" do
         context "on run " do
             it "returns a string" do
                 expect(ElevatorMedia::Streamer.getContent()).to be_a(String)
+            end
+        
+        end
+    end
+    describe ".api_request" do
+        context "sends 0 to the joke api" do
+            it "returns a specific joke as json object" do
+                expect(ElevatorMedia::Streamer.api_request("0")).to eq(JSON.parse({
+                    "error": false,
+                    "category":"Programming",
+                    "type":"single",
+                    "joke":"I've got a really good UDP joke to tell you but I don’t know if you'll get it.",
+                    "flags":{
+                            "nsfw":false,
+                            "religious":false,
+                            "political":false,
+                            "racist":false,
+                            "sexist":false
+                            },
+                    "id": 0,
+                    "lang": "en"
+                        }.to_json))
+           
             end
         end
     end
