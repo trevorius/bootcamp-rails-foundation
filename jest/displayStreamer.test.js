cc0a1eddc0d894f9128c13e812b4fd6bac8b7c72
trevorius <trevorius45@hotmail.com>
2020-11-26 21:56:35 -0500

    refactored code  and added comments

--- jest/displayStreamer.test.js
+++ jest/displayStreamer.test.js
@@ -1,18 +1,12 @@
 const Streamer = require('../lib/elevatorMedia.js');
 // const $ = require('jquery');
 let example = new Streamer();
-
 const joke0 = require('../fixtures/joke0response.json')
 const joke147 = require('../fixtures/joke147response.json')
 
-
-
-
 // verify class import works
 test('test function', () => {
-
         expect(example.testing("string")).toBe("test string")
-
 });
 
     
@@ -24,15 +18,15 @@ test('joke api call by verrifying the language of the joke.', async () => {
     // verrifie the property of the returned body .
     expect(data).toHaveProperty("lang", "en"); 
 });
+
 test ('joke value ', async () =>{
-
+    // break the JSON down and make sure it is valid.
     var subject = await example.jokeJson;
-
     expect(subject).toHaveProperty("lang", "en");
 });
 
 test('extractJoke with a one liner', () => {
-
+    // there are  two types of joke and this test will check oneliners 
     expect(example.extractJoke(joke0)).toBe
         ("I've got a really good UDP joke to tell you but I don’t know if you'll get it.")
 });
@@ -44,16 +38,17 @@ test('extractJoke with a two liner', () => {
                     '"second-line">But the look of joy in his eyes whenever I answer him is worth the world.'
     expect(example.extractJoke(joke147)).toBe(twoLiner)
 });
-test(' make joke into html string (makeHtmlString)',()=>{
 
+test(' make joke into html string (makeHtmlString)',()=>{
     expect(example.makeHtmlString("string")).toMatch(/<div/)   
 });
 test ('getContent', async () =>{
-    
+    // test the final streamer method
     const test = await  example.getContent("0-150");
     expect(typeof test).toBe('string');
 });
-test ('getContent as a joke ', async ()  => {
+test ('getContent is html format', async ()  => {
+    // if the string contains the div tags it will display on a html page.
     const test = await  example.getContent("0-150");
     expect(test).toMatch(/<div/)
 
